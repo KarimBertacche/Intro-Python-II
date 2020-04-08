20090707771292530652eda0a61f60ce94ec140a
Karim B <mattiakarim@live.it>
2020-04-08 14:29:42 +0200

    Reformatted room dictionary to be more readable

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,29 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'outside': Room(
+        "Outside Cave Entrance",
+        "North of you, the cave mount beckons"
+    ),
+    'foyer': Room(
+        "Foyer", 
+        """Dim light filters in from the south. Dusty passages run north and east."""
+    ),
+    'overlook': Room(
+        "Grand Overlook", 
+        """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""
+    ),
+    'narrow': Room(
+        "Narrow Passage", 
+        """The narrow passage bends here from west to north. The smell of gold permeates the air."""
+    ),
+    'treasure': Room(
+        "Treasure Chamber", 
+        """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""
+    ),
 }
 
 
@@ -38,9 +43,13 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+new_player = Player("Karim", "outside")
+
+print(new_player)
 
 # Write a loop that:
 #
+
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
