9ee7f6696cc07663f0bf73b34dd5e2481aa5761a
Karim B <mattiakarim@live.it>
2020-04-08 13:59:30 +0200

    Print Room class and added human readable version of print out

--- src/room.py
+++ src/room.py
@@ -7,7 +7,3 @@ class Room:
 
     def __str__(self):
         return f"name: {self.name}, description: {self.description}"
-
-room = Room("Only Cow", "Great room with awesome things in it, bigly and great")
-
-print(room)
