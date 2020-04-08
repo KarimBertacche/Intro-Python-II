82b9f348427b92f8c8b16fe087a21a086c313764
Karim B <mattiakarim@live.it>
2020-04-08 15:59:27 +0200

    Used input to get player name within Player class instance

--- src/adv.py
+++ src/adv.py
@@ -45,7 +45,7 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-new_player = Player("Karim")
+player = Player(input("Please enter your name: "))
 
 print(new_player)
 
