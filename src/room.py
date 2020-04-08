7d19995627f5cda95e9019068843772f9e60dc17
Karim B <mattiakarim@live.it>
2020-04-08 13:58:34 +0200

    Create Room class and added attributes for both name and description

--- src/room.py
+++ src/room.py
@@ -1,2 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f"name: {self.name}, description: {self.description}"
+
+room = Room("Only Cow", "Great room with awesome things in it, bigly and great")
+
+print(room)
